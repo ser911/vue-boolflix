366df3c0f8f98c60cd6781109fea36f76a6da932
Sergio <sergio.marretta@libero.it>
2021-04-12 18:11:10 +0200

    broken

--- js/myscript.js
+++ js/myscript.js
@@ -2,20 +2,69 @@ var app = new Vue (
   {
 el: "#app",
 data:{
-
+search: "",
+tempMovieArray: [],
+tempTvArray: [],
+apiKey: "8b919efbe3d00d2c300a4f551dda0d42",
+languages: []
 },
 methods: {
 moviesearch: function(){
-  axios.get("https://api.themoviedb.org/3/movies/550?api_key=8b919efbe3d00d2c300a4f551dda0d42").then((result) =>{
-console.log(result.data.response);
-    
+  axios.get("https://api.themoviedb.org/3/search/movie", {
+
+    params: {
+      api_key: this.apiKey,
+      query: this.search
+    }
+  }).then((result) =>{
+  this.tempMovieArray = result.data.results;
+console.log(this.tempMovieArray);
+
   }
 )
+
+axios.get("https://api.themoviedb.org/3/search/tv",
+{
+params: {
+  api_key: this.apiKey,
+  query: this.search
+}
+
+
+}).then((result) =>{
+  this.tempMovieArray = result.data.results;
+console.log(this.tempMovieArray);
+}
+
+
+
+)
+axios.get("https://api.themoviedb.org/3/search",
+{
+params: {
+  api_key: this.apiKey,
+  query: this.search
+}
+
+}).then((result) =>{
+  this.languages = result.data.results.original_language;
+console.log(this.languages);
+}
+
+
+
+)
+
+
+  }
+
+
+
 }
 
 
 }
 
 
-}
+
 );
